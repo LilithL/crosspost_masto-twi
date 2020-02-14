213b605a60652d0cec4f06c2e996174e80224ac4
LilithL <louise@louisem.space>
2020-02-14 13:10:07 +0100

    Added mastodon utils file

---
+++ MastoCrosspostUtils.py
@@ -0,0 +1,94 @@
+from bs4 import BeautifulSoup
+from mastodon import Mastodon
+import html
+from urllib.parse import urlparse
+
+
+class MastoCrosspostUtils:
+
+    def __init__(self, clientcred_key, access_token_key, instance_url):
+        self.mastodon_api = Mastodon(
+            client_id=clientcred_key,
+            access_token=access_token_key,
+            api_base_url=instance_url
+        )
+        self.me = self.mastodon_api.account_verify_credentials()
+
+    def scrape_toots(self, mstdn_acct_id, since=None):
+        """ Get toots from an account since given toot id and filter them
+        """
+        toots = self.mastodon_api.account_statuses(
+            mstdn_acct_id, since_id=since, exclude_replies=True)
+        filtered_toots = []
+        if len(toots):
+            filtered_toots = list(filter(lambda x:
+                                         x['reblog'] is None and
+                                         x['poll'] is None and
+                                         x['visibility'] in [
+                                             "public", "unlisted", "private"],
+                                         toots[::-1]))
+        return filtered_toots
+
+    def get_following(self):
+        return self.mastodon_api.account_following(self.me.id)
+
+
+def process_toot_to_chunks(toot_content, size):
+    return str_to_chunks(strip_tags(toot_content), size)
+
+
+def strip_tags(content):
+    """ Strip html tags from a given text (ere, we use it to strip tags from toots)
+    """
+    soup = BeautifulSoup(html.unescape(content), 'html.parser')
+
+    # Removes cards
+    tags = soup.select('.h-card')
+    for i in tags:
+        i.replace_with(i.next_element)
+
+    # Transforms # to text
+    tags = soup.select('.hashtag')
+    for i in tags:
+        i.replace_with(i.get_text())
+
+    # transform mentions text like "user@example.com"
+    tags = soup.select('.mention')
+    for i in tags:
+        mention_acct_url = urlparse(i["href"])
+        i.replace_with(
+            mention_acct_url.path[2:] + "@" + mention_acct_url.netloc)
+
+    # clear shortened link captions
+    tags = soup.select('.invisible, .ellipsis')
+    for i in tags:
+        i.unwrap()
+
+    # replace link text to avoid caption breaking
+    tags = soup.select('a')
+    for i in tags:
+        i.replace_with(i.get_text())
+
+    # strip html tags, chr(31) joins text in different html tags
+    return soup.get_text('\n').strip()
+
+
+def str_to_chunks(string, size):
+    """ Construct a list of strings with a given max size for each strings.
+    """
+    res = [""]
+    words = string.split()
+    i = 0
+    while len(words):
+        if len(res[i]) + len(words[0]) < size:
+            res[i] = res[i] + " " + words[0]
+            words = words[1:]
+        else:
+            i += 1
+            if len(words[0]) >= size:
+                res.append(words[0][:size - 1] + "-")
+                words[0] = words[0][size - 1:]
+            else:
+                res.append("")
+
+    return res
--- crosspost_masto-twi.py
+++ crosspost_masto-twi.py
@@ -1,13 +1,10 @@
 import configparser
-import html
 import json
 import time
-from urllib.parse import urlparse
 
 import click
 import twitter
-from bs4 import BeautifulSoup
-from mastodon import Mastodon
+import MastoCrosspostUtils
 
 # The global var that'll contain the parameters from the config file
 PARAMS = {}
@@ -29,83 +26,9 @@ def import_json_file(file_name):
         exit()
 
 
-def str_to_chunks(string, size):
-    """ Construct a list of strings with a given max size for each strings.
-    """
-    res = [""]
-    words = string.split()
-    i = 0
-    while len(words):
-        if len(res[i]) + len(words[0]) < size:
-            res[i] = res[i] + " " + words[0]
-            words = words[1:]
-        else:
-            i += 1
-            if len(words[0]) >= size:
-                res.append(words[0][:size - 1] + "-")
-                words[0] = words[0][size - 1:]
-            else:
-                res.append("")
-
-    return res
-
-
-def strip_tags(content):
-    """ Strip html tags from a given text (ere, we use it to strip tags from toots)
-    """
-    soup = BeautifulSoup(html.unescape(content), 'html.parser')
-
-    # Removes cards
-    tags = soup.select('.h-card')
-    for i in tags:
-        i.replace_with(i.next_element)
-
-    # Transforms # to text
-    tags = soup.select('.hashtag')
-    for i in tags:
-        i.replace_with(i.get_text())
-
-    # transform mentions text like "user@example.com"
-    tags = soup.select('.mention')
-    for i in tags:
-        mention_acct_url = urlparse(i["href"])
-        i.replace_with(
-            mention_acct_url.path[2:] + "@" + mention_acct_url.netloc)
-
-    # clear shortened link captions
-    tags = soup.select('.invisible, .ellipsis')
-    for i in tags:
-        i.unwrap()
-
-    # replace link text to avoid caption breaking
-    tags = soup.select('a')
-    for i in tags:
-        i.replace_with(i.get_text())
-
-    # strip html tags, chr(31) joins text in different html tags
-    return soup.get_text('\n').strip()
-
-
-def scrape_toots(mastodon_api, mstdn_acct_id, since=None):
-    """ Get toots from an account since given toot id and filter them
-    """
-    toots = mastodon_api.account_statuses(
-        mstdn_acct_id, since_id=since, exclude_replies=True)
-    filtered_toots = []
-    if len(toots):
-        filtered_toots = list(filter(lambda x:
-                                     x['reblog'] is None and
-                                     x['poll'] is None and
-                                     x['visibility'] in [
-                                         "public", "unlisted", "private"],
-                                     toots[::-1]))
-    return filtered_toots
-
-
 def tweet_parser(twi_api, texte, toot_cont_warn, toot_media=None):
     """ Construct tweets based on a toot content and cw and tweet it
     """
-    texte = strip_tags(texte)
     if toot_cont_warn != "":
         formated_cw = PARAMS['twi_cw_default'].replace(
             "\\n", "\n").format(toot_cont_warn)
@@ -129,7 +52,7 @@ def tweet_parser(twi_api, texte, toot_cont_warn, toot_media=None):
         last_tweet_id = None
 
     if not any(word in toot_cont_warn.lower() for word in PARAMS['no_cp_indicators'].split(',')):
-        texte = str_to_chunks(
+        texte = MastoCrosspostUtils.process_toot_to_chunks(
             texte, int(PARAMS['twi_limit']) - len(formated_cw) - url_lenght)
         for contenu in texte:
             try:
@@ -141,21 +64,22 @@ def tweet_parser(twi_api, texte, toot_cont_warn, toot_media=None):
     return
 
 
-def tweet_last_toots(mastodon_api, twi_api, acct):
+def tweet_last_toots(mastodon_utils, twi_api, acct):
     """ Get the last toots from a given account id and post it on twitter
     """
     try:
         with open(PARAMS['last_toot_file'], 'r') as f:
             l_toot_json = json.load(f)
         if str(acct.id) not in l_toot_json:
-            l_toot_json[str(acct.id)] = scrape_toots(
-                mastodon_api, acct.id, None)[-1]['id']
+            l_toot_json[str(acct.id)] = mastodon_utils.scrape_toots(
+                acct.id, None)[-1]['id']
     except Exception as e:
         print(e)
         l_toot_json = {}
-        l_toot_json[str(acct.id)] = scrape_toots(
-            mastodon_api, acct.id, None)[-1]['id']
-    last_toots = scrape_toots(mastodon_api, acct.id, l_toot_json[str(acct.id)])
+        l_toot_json[str(acct.id)] = mastodon_utils.scrape_toots(
+            acct.id, None)[-1]['id']
+    last_toots = mastodon_utils.scrape_toots(
+        acct.id, l_toot_json[str(acct.id)])
     if len(last_toots) > 0:
         if l_toot_json[str(acct.id)] != last_toots[-1].id:
             for new_toot in last_toots:
@@ -186,11 +110,8 @@ def run(conf, delay):
     PARAMS = configParser._sections['config-crosspost']
 
     usercred_masto = import_json_file(PARAMS['masto_usercred_file'])
-    mastodon_api = Mastodon(
-        client_id=PARAMS['masto_clientcred_file'],
-        access_token=usercred_masto['token'],
-        api_base_url=usercred_masto['url']
-    )
+    mastodon_utils = MastoCrosspostUtils.MastoCrosspostUtils(
+        PARAMS['masto_clientcred_file'], usercred_masto['token'], usercred_masto['url'])
 
     accesstoken_twitter = import_json_file(PARAMS['twi_accesstoken_file'])
     consumerkey_twitter = import_json_file(PARAMS['twi_consumerkey_file'])
@@ -203,11 +124,10 @@ def run(conf, delay):
 
     while 1:
         if PARAMS['is_external_acct'].lower() == 'true':
-            for mstdn_acct in mastodon_api.account_following(mastodon_api.account_verify_credentials().id):
-                tweet_last_toots(mastodon_api, twi_api, mstdn_acct)
+            for mstdn_acct in mastodon_utils.get_following():
+                tweet_last_toots(mastodon_utils, twi_api, mstdn_acct)
         elif PARAMS['is_external_acct'].lower() == 'false':
-            tweet_last_toots(mastodon_api, twi_api,
-                             mastodon_api.account_verify_credentials().id)
+            tweet_last_toots(mastodon_utils, twi_api, mastodon_utils.me.id)
         else:
             raise Exception(
                 "Parameter 'is_external_acct' is undefined in config file")
@@ -239,7 +159,9 @@ def print_conf_to_file(output):
                     "twi_consumerkey_file = twitter_consumer_key.secret\n\n" +
                     "# The path to the mastodon api tokens\n" +
                     "masto_clientcred_file = clientcred.secret\n" +
-                    "masto_usercred_file = usercred.secret\n")
+                    "masto_usercred_file = usercred.secret\n\n" +
+                    "# The sqlite3 file name\n" +
+                    "sqlite_file = toots.db")
     except Exception as e:
         print(e)
         exit(1)
